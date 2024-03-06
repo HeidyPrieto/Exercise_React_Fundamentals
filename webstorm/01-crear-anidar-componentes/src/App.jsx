ff80bf012182c616958f722a6543061a9a933c2d
HeidyPrieto <140523540+HeidyPrieto@users.noreply.github.com>
2024-03-06 17:12:15 -0500

    (FrontEnd):Finish the Activity 1 about create and nest components

---
+++ webstorm/01-crear-anidar-componentes/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+function MyButton(){
+    return(
+        <button> Soy un botón</button>
+    );
+}
+
+function App(){
+    return(
+        <div>
+            <h1>Bienvenidos a mi pagina</h1>
+            <MyButton/>
+        </div>
+    );
+}
+
+export default App;
