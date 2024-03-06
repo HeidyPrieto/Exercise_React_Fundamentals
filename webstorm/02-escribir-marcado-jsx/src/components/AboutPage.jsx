bd1274ff5255db6b6a34c3c0e53f36914801fa8d
HeidyPrieto <140523540+HeidyPrieto@users.noreply.github.com>
2024-03-06 17:48:14 -0500

    (FrontEnd):Finish Activity 2 create folder components

---
+++ webstorm/02-escribir-marcado-jsx/src/components/AboutPage.jsx
@@ -0,0 +1,10 @@
+function AboutPage(){
+    return(
+        <div>
+            <h1>About</h1>
+            <p>Hello there. <br />How do you do?</p>
+        </div>
+    );
+}
+
+export default AboutPage;
---
+++ webstorm/02-escribir-marcado-jsx/src/components/MyButton.jsx
@@ -0,0 +1,7 @@
+function MyButton(){
+    return(
+        <button className={"button"}> Soy un botón</button>
+    );
+}
+
+export default MyButton;
